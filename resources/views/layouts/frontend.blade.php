240db183674a3e89102771f139d8fa9b1c697b52
Abyravka90 <asepcahya.nugraha75@gmail.com>
2024-02-13 10:23:11 +0700

    initial commit

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+<head></script>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.118.2">
+  <title>12 rpl 2 - Blog</title>
+  @include('includes.public.style')
+</head>
+<body>
+@yield('custom-style')
+
+@include('includes.public.header')
+
+@yield('content')
+
+@include('includes.public.footer')
+@include('includes.public.script')
+</body>
+</html>
